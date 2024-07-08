0e1ab63617e6dea97ac5fa33ec46fb301f5a246e
David Alves Dutra <dadutra@hotmail.com>
2024-07-08 19:01:40 -0300

    Validação do formulário. Card03 parte 1.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login = {
+    email: "",
+    senha: ""
+  };
 
+  entrar() {
+    if ((this.login.email != null) && (this.login.senha != null)) {
+      window.location.href = "/home";
+    } else {
+      alert('Campos em branco!');
+    }
+  }
+
+  recuperarSenha() {
+    alert('Processo de recuperação de senha enviado para o e-mail cadastrado');
+  }
 }
