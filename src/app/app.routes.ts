322656a74efb12f809fb5b156f5a49f53b564fd0
David Alves Dutra <dadutra@hotmail.com>
2024-07-08 10:12:47 -0300

    Criadas as rotas para cada componente. Card02.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { CadastroAlunoComponent } from './cadastro-aluno/cadastro-aluno.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'alunos', component: AlunosComponent},
+    {path: 'cadastroAlunos', component: CadastroAlunoComponent},
+    {path: 'disciplinas', component: DisciplinasComponent}
+];
