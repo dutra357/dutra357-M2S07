e5b91f8033c906527c93f4054bed4541dddf10a4
David Alves Dutra <dadutra@hotmail.com>
2024-07-09 09:45:22 -0300

    Formulário para cadastro de alunos. Card04.

--- src/app/cadastro-aluno/cadastro-aluno.component.ts
+++ src/app/cadastro-aluno/cadastro-aluno.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-aluno',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './cadastro-aluno.component.html',
   styleUrl: './cadastro-aluno.component.css'
 })
 export class CadastroAlunoComponent {
+
+  cadastro = {
+    nome: "",
+    cpf: "",
+    email: "",
+    celular: "",
+    curso: ""
+  };
+
+  salvar() {
+    if (this.cadastro.nome && this.cadastro.cpf
+      && this.cadastro.email && this.cadastro.celular
+      && this.cadastro.curso) {
+      
+        window.location.href = "/alunos";
+        alert('Cadastro realizado com sucesso!');
+    } else {
+      alert('Campos incompletos.');
+    }
+  }
+
+  recuperarSenha() {
+    alert('Processo de recuperação de senha enviado para o e-mail cadastrado');
+  }
 
 }
