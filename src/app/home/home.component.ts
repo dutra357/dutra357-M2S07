09dc4e8377ed4d65544110d280efd7ecdaea034a
David Alves Dutra <dadutra@hotmail.com>
2024-07-11 17:44:11 -0300

    Card07. Home com cards de atividades pendentes, disciplinas e cursos. ngFor.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  aluno = "João Silveira";
+
+  atividades = [
+    'atividade01', 'atividade02', 'atividade03', 'atividade04'
+  ];
+
+  extras = [
+    'Língua Estrangeira', 'Programação - JAVA', 'Seminário' 
+  ];
+
+  disciplinas = [
+    'Cálculo I', 'Cálculo II', 'Mecânica dos Sólidos' 
+  ];
 
 }
