11a9fec7d1dd597b78bd48eef7cfc1a63455d9c0
David Alves Dutra <dadutra@hotmail.com>
2024-07-11 19:12:31 -0300

    Card01 - EXERCICIO 8. COntinuação. Header.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/shared/components/header/header.component.ts
+++ src/shared/components/header/header.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  constructor(private router: Router) { }
+
+  sair() {
+
+    let confirmacao = confirm("Deseja realmente sair?");
+    if (confirmacao) {
+      //deslogar
+      this.router.navigate(["login"]);
+    }
+  }
+
+  path = 'https://img.icons8.com/?size=100&id=42763&format=png&color=000000'
+  usuario = 'Joao Silveira'
 
 }
